b974d13ad2ef65050cfc77f04997e67918acabfd
Kulkarni Varun Uddhav <kulkarnivarunu02@gmail.com>
2025-02-07 22:59:35 +0530

    ETL Command Tested

--- Test-command.sql
+++ Test-command.sql
@@ -12,7 +12,17 @@ WHERE a.region='ca';
 
 ---
 
+SELECT a.title,a.category_id,b.snippet_title 
+FROM "db-yt-clean"."raw_statistics" a 
+INNER JOIN "db-yt-clean"."clean_stat_ref_data" b 
+ON a.category_id=b.id
+WHERE a.region='ca';
 
+SELECT * 
+FROM "db-yt-clean"."raw_statistics" a 
+INNER JOIN "db-yt-clean"."clean_stat_ref_data" b 
+ON a.category_id=b.id
+WHERE a.region='ca';
 
 ---
 
