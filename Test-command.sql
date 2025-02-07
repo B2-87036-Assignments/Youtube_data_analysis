e808148cd1c10d9d19c5fe7e722098076613ee1e
prakharnema <prakharnema07@gmail.com>
2025-02-07 22:51:08 +0530

    lambda function testing done

---
+++ Test-command.sql
@@ -0,0 +1,19 @@
+SELECT a.title,a.category_id,b.snippet_title 
+FROM "db-yt-raw"."raw_statistics" a 
+INNER JOIN "db-yt-raw"."clean_stat_ref_data" b 
+ON a.category_id=CAST(b.id as int)
+WHERE a.region='ca';
+
+SELECT a.title,a.category_id,b.snippet_title 
+FROM "db-yt-raw"."raw_statistics" a 
+INNER JOIN "db-yt-raw"."clean_stat_ref_data" b 
+ON a.category_id=b.id
+WHERE a.region='ca';
+
+---
+
+
+
+---
+
+
